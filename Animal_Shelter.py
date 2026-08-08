b4b8b814534614c895c89e4902f8f30cb9482544
charlesdmg <mendezgutierrezcharlesd@gmail.com>
2026-08-08 19:36:55 -0400

    First class

---
+++ Animal_Shelter.py
@@ -0,0 +1,13 @@
+class Animal:
+    def __init__(self, name: str, type: str):
+        self.name = name
+        self.type = type
+
+    def make_sound():
+        print("The animal makes a sound")
+
+    def feed():
+        print("The animal eats")
+
+    def sleep():
+        print("The animal sleeps")
